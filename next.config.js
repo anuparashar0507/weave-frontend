43bca23b9a4235703f35f91a09b38b0b0836561f
Anupam Parashar <aparashar@zenon.ai>
2023-02-22 19:59:07 +0530

    config reset

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: "file-loader",
-          options: {
-            publicPath: "assets",
-          },
-        },
-      ],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
